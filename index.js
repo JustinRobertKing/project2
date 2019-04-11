c46f744807f07a18b52d5b4a59520b229bb74207
JustinRobertKing <kingj4@spu.edu>
2019-04-11 11:53:37 -0700

    layout & home ejs working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 // Require necessary modules
 let express = require('express')
+let layouts = require('express-ejs-layouts')
 
 // Declare Express app
 let app = express()
@@ -11,12 +12,13 @@ let app = express()
 app.set('view engine', 'ejs')
 
 // Include (use) middle ware
+app.use(layouts)
 
 // Include routes from controllers
 
 // Make a home route: GET /
 app.get('/', (req, res) => {
-	res.send('Home')
+	res.render('home')
 })
 
 // Listen from your port
