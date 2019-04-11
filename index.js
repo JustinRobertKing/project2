84be326d965d4538fc53ec4e07fc2521873f862a
JustinRobertKing <kingj4@spu.edu>
2019-04-11 11:35:01 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Include .env variables
+
+// Require necessary modules
+
+// Declare Express app
+
+// Set view engin
+
+// Include (use) middle ware
+
+// Include routes from controllers
+
+// Make a home route: GET /
+
+// Listen from your port
